cea5e3823b7d826902b28975923af1e09825c4c6
Tom Dalby <thomasjackdalby@gmail.com>
2025-12-21 17:28:22 +0000

    Updated template for rust

--- templates/rust/src/main.rs
+++ templates/rust/src/main.rs
@@ -8,10 +8,20 @@ use std::env;
 
 fn main() {{
     let args: Vec<String> = env::args().collect();
-    let file_path: &str = if args.len() > 0 {{ &args[1]; }} else {{ "input.txt"; }}
+    let file_path: &str = if args.len() > 1 {{ &args[1] }} else {{ "input.txt" }};
     println!("file_path: {{file_path}}");
+    let contents = fs::read_to_string(file_path).unwrap();
 
+    println!("part_1: {{}}", part_1(&contents));
+    println!("part_2: {{}}", part_2(&contents));
+}}
+
+fn part_1(contents: &str) {{
     let mut result: u32 = 0;
+    return result;
+}}
 
-    println!("{{result}}");
+fn part_2(contents: &str) {{
+    let mut result: u32 = 0;
+    return result;
 }}
